f306adacb3d00a3af5c51b7116e22dbb8caa8afe
Yixuan Sun <yixuan.purdue@gmail.com>
2023-02-28 09:33:02 -0500

    removed cuda options in py-numpyro

--- packages/py-numpyro/package.py
+++ packages/py-numpyro/package.py
@@ -17,17 +17,13 @@ class PyNumpyro(PythonPackage):
     # Versions
     version("0.10.1", sha256="dfa896f01c8df43ddd41663af2b3e2fc4aea73489e669474e83329b183b29150")
 
-    # Variants
-    variant("cuda", default=False, description="CUDA support")
 
     # Dependencies
     depends_on("python@3.7:", type=("build", "run"))
 
     depends_on("py-setuptools", type="build")
 
-    depends_on("py-jax@0.2.13:~cuda", type=("build", "run"), when="~cuda")
-    depends_on("py-jax@0.2.13:+cuda", type=("build", "run"), when="+cuda")
-    # depends_on("py-jaxlib@0.1.65:~cuda", type=("build", "run"), when="~cuda")
+    depends_on("py-jax@0.2.13:", type=("build", "run"))
     depends_on("py-multipledispatch", type=("build", "run"))
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-tqdm", type=("build", "run"))
